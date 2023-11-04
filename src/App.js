40b8532e627b601db9a56138bf89786d6a8955ac
Ryan <ryanpvoong@gmail.com>
2023-11-03 21:37:25 -0400

    removed unused code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import Navbar from "./components/navbar";
-import { useLocation, useParams, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import BlogList from "./pages/blogList";
 import MainPage from "./components/mainPage";
 import VideoStreamerBlog from "./components/video-streamer-blog";
 
 
 function App() {
-  const pathname= useLocation();
-  const params = useParams();
-  console.log("pathname: ", pathname.pathname)
-  console.log("useparams: ", params)
-
   return (
     <>
       <div className="sticky top-0">
@@ -19,9 +14,8 @@ function App() {
 
       <Routes>
         <Route exact path='/' element={ <MainPage /> } />
-        <Route path="/blogs" element={ <BlogList /> } >
-        </Route>
-          <Route path="/blogs/item1" element={ <VideoStreamerBlog /> } />
+        <Route path="/blogs" element={ <BlogList /> } />
+        <Route path="/blogs/item1" element={ <VideoStreamerBlog /> } />
       </Routes>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
   <React.StrictMode>
 
     <BrowserRouter>
@@ -14,5 +13,4 @@ root.render(
     </BrowserRouter>
 
   </React.StrictMode>
-
 );
