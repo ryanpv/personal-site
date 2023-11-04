214cffea18fa128370a9cae32258005626062855
Ryan <ryanpvoong@gmail.com>
2023-11-03 22:03:52 -0400

    changed formatting for blog list

--- src/pages/blogList.js
+++ src/pages/blogList.js
@@ -4,16 +4,27 @@ export default function BlogList() {
 
   return(
     <>
-      <h1 className="text-5xl font-extrabold leading text-white underline text-center">Blog List</h1>
-      <ul>
-        <li className="text-5xl font-extrabold leading text-white underline text-center">
-          <a href="/blogs/item1">
-            ExpressJS Video Streaming Server with AWS
-            </a>
-        </li>
-        <li className="text-5xl font-extrabold leading text-white underline text-center">Item 2</li>
-        <li className="text-5xl font-extrabold leading text-white underline text-center">Item 3</li>
-      </ul>
+      <div className='flex h-full flex-col'>
+        <main className='flex justify-center items-center p-16 mb-auto'>
+          <div className='max-w-4xl flex flex-col divide-y divide-gray-200 dark:divide-gray-700'>
+            <div className='space-y-2 pt-6 pb-8 md:space-y-5'>
+            <header className='text-5xl font-extrabold leading text-white underline text-center'>
+                My Personal Blogs
+              </header>
+            <p className='text-white text-center'>Check out my blogs and feel free to ask questions/send some feedback!</p>
+            </div>
+
+            <div className='container'>
+              <li className="text-2xl font-extrabold leading text-white underline text-center">
+                <a href="/blogs/item1">
+                  ExpressJS Video Streaming Server with AWS
+                </a>
+              </li>
+            </div>
+
+          </div>
+        </main>
+      </div>
     </>
   )
 }
