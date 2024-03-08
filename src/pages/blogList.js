c9e54495f1af59e6ca9721e551e268097d190669
Ryan <ryanpvoong@gmail.com>
2024-03-08 18:43:39 -0500

    updated styling for bloglist and blog

--- src/pages/blogList.js
+++ src/pages/blogList.js
@@ -8,10 +8,10 @@ export default function BlogList() {
         <main className='flex justify-center items-center p-16 mb-auto'>
           <div className='max-w-4xl flex flex-col divide-y divide-gray-200 dark:divide-gray-700'>
             <div className='space-y-2 pt-6 pb-8 md:space-y-5'>
-              <header className='text-5xl font-extrabold leading text-white underline text-center'>
+              <header className='text-5xl font-extrabold leading text-white underline decoration-orange-400 text-center'>
                 My Personal Blogs
               </header>
-              <p className='text-white text-center'>Check out my blogs and feel free to ask questions/send some feedback!</p>
+              <p className='text-white text-center font-semibold'>Check out my blogs and feel free to ask questions/send some feedback!</p>
             </div>
 
             <div className='container'>
